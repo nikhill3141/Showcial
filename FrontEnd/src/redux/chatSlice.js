964a01ef96d8995cc04a78a2f6a503dba8b6b73c
Nikhil <nikhilmali3141@gmail.com>
2026-02-01 14:26:52 +0530

    changed chatSlice (restore)

--- FrontEnd/src/redux/chatSlice.js
+++ FrontEnd/src/redux/chatSlice.js
@@ -1,41 +1,41 @@
-import { createSlice } from "@reduxjs/toolkit";
-
-
-const chatSlice = createSlice({
-  name:'chat',
-  initialState:{
-    onlineUsers:[],
-    messages:[]
-  },
-  reducers:{
-    setOnlineUsers:(state,action) => {
-      state.onlineUsers = action.payload
-    },
-    setMessages:(state,action) => {
-      state.messages = action.payload
-    }
-  }
-})
-export const {setOnlineUsers, setMessages} = chatSlice.actions
-export default chatSlice.reducer
-
 // import { createSlice } from "@reduxjs/toolkit";
 
+
 // const chatSlice = createSlice({
-//     name:"chat",
-//     initialState:{
-//         onlineUsers:[],
-//         messages:[],
+//   name:'chat',
+//   initialState:{
+//     onlineUsers:[],
+//     messages:[]
+//   },
+//   reducers:{
+//     setOnlineUsers:(state,action) => {
+//       state.onlineUsers = action.payload
 //     },
-//     reducers:{
-//         // actions
-//         setOnlineUsers:(state,action) => {
-//             state.onlineUsers = action.payload;
-//         },
-//         setMessages:(state,action) => {
-//             state.messages = action.payload;
-//         }
+//     setMessages:(state,action) => {
+//       state.messages = action.payload
 //     }
-// });
-// export const {setOnlineUsers, setMessages} = chatSlice.actions;
-// export default chatSlice.reducer;
+//   }
+// })
+// export const {setOnlineUsers, setMessages} = chatSlice.actions
+// export default chatSlice.reducer
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const chatSlice = createSlice({
+    name:"chat",
+    initialState:{
+        onlineUsers:[],
+        messages:[],
+    },
+    reducers:{
+        // actions
+        setOnlineUsers:(state,action) => {
+            state.onlineUsers = action.payload;
+        },
+        setMessages:(state,action) => {
+            state.messages = action.payload;
+        }
+    }
+});
+export const {setOnlineUsers, setMessages} = chatSlice.actions;
+export default chatSlice.reducer;
